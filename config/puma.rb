4e35c28b322107c0dce63c21aafe260ea31d53c6
svijaya7 <vijayaksa@advisory.com>
2020-03-29 22:14:01 +0530

    remove active record from puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -54,9 +54,3 @@ stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.std
 pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
-
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
