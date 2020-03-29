d57c89e9b6bfe60e6f415e4610f9f9bf1a450d1e
svijaya7 <vijayaksa@advisory.com>
2020-03-29 18:41:36 +0530

    remove ssh_option for git on deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,12 +28,6 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, false
 
-set :ssh_options, {
-    user: 'svijaya7',
-    auth_methods: %w(publickey),
-    keys: %w(~/.ssh/budget-planner-bitbucket.pub)
-}
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
