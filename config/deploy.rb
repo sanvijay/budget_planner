b02f563586e9cbd453ffc70cf453600bf3b680ff
svijaya7 <vijayaksa@advisory.com>
2020-04-04 22:11:10 +0530

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,34 +63,3 @@ set :ssh_options, {
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  after  :finishing, :cleanup
-end
